b88c8ec9a94b7af86cc9fd12bde94e3d774b8e98
juancwu <46619361+juancwu@users.noreply.github.com>
2023-03-23 19:53:07 -0400

    wrap vgg19 with custom model class

--- src/models/vgg19_model.py
+++ src/models/vgg19_model.py
@@ -1,61 +1,68 @@
-from keras.layers import Dropout, GlobalAveragePooling2D, Dense 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import os
-
-from sklearn.model_selection import train_test_split
-from keras.utils import to_categorical
+from keras.layers import GlobalAveragePooling2D, Dense
 from keras.applications.vgg19 import VGG19
-
-vgg19_base = VGG19(weights='imagenet', include_top=False,
-                   input_shape=(img_size, img_size, 3))
-x1 = vgg19_base.output
-x1 = tf.keras.layers.GlobalAveragePooling2D()(x1)
-x1 = tf.keras.layers.Dense(1024, activation='relu')(x1)
-x1 = tf.keras.layers.Dropout(0.5)(x1)
-predictions = tf.keras.layers.Dense(len(classes), activation='softmax')(x1)
-
-model = Model(inputs=[vgg19_base.input], outputs=[predictions])
-
-for layer in vgg19_base.layers:
-    layer.trainable = False
-
-model.compile(optimizer='adam', loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-batch_size = 32
-epochs = 30
-
-history = model.fit(
-    train_X,
-    train_Y,
-    batch_size=batch_size,
-    epochs=epochs,
-    verbose=1,
-    validation_data=(test_X, test_Y)
-)
-model.summary()
-
-score = model.evaluate(test_X, test_Y, verbose=0)
-print("loss:", score[0])
-print("accuracy:", score[1])
-
-# model.save("/some/path")
+from keras.models import Model
 
 class DetectorModelBaseVGG19:
     def __init__(self, weights='imagenet', include_top=False, input_shape=(224, 224, 3), classes=["no", "yes"]):
         self.vgg19 = VGG19(weights=weights, include_top=include_top, input_shape=input_shape)
-        self.classes = [] 
+        self.classes = [cl for cl in classes] 
+        self.model = None
+        self.custom_layers = []
+        self.fitted = False
+        self.compiled = False
         
-        # create new layers with vgg19 as a base
+    def build(self):
+        for layer in self.vgg19.layers:
+            layer.trainable = False
+
         x = self.vgg19.output
         x = GlobalAveragePooling2D()(x)
-        x = Dense(1024, activation='relu')(x)
-        x = Dropout(0.5)(x)
 
-        predictions = Dense(len(self.classes), activation='softmax')(x)
+        if len(self.custom_layers):
+            for layer in self.custom_layers:
+                x = layer(x)
+
+        # sigmoid is logistic function, suited for binary classification
+        predictions = Dense(1, activation='sigmoid')(x)
+
+        self.model = Model(inputs=self.vgg19.input, outputs=predictions)
+
     
-    def fit(train_X, trian_Y, test_X, test_Y, batch_size=32, epochs=30, verbose=1):
-        pass
+    def fit(self, train_X, train_Y, test_X, test_Y, batch_size=32, epochs=30, verbose=1):
+        assert self.model is not None, "Build model before fitting."
+        assert not self.fitted, "Model already fitted."
+        assert self.compiled, "Must compile model before fitting."
 
+        history = self.model.fit(
+                    train_X,
+                    train_Y,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=verbose,
+                    validation_data=(test_X, test_Y))
+
+        self.fitted = True
+
+        return history
+
+    def compile(self, optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"]):
+        assert self.model is not None, "Build model before compiling."
+        assert not self.compiled, "Model already compiled."
+
+        self.model.compile(
+                optimizer=optimizer,
+                loss=loss,
+                metrics=metrics
+                )
+        self.compiled = True
+
+    def add(self, layer):
+        self.custom_layers.append(layer)
+
+        return self
+
+    def save(self, save_path):
+        self.model.save(save_path)
+
+    def score(self, test_X, test_Y, verbose=0):
+        return self.model.evaluate(test_X, test_Y, verbose=verbose)
