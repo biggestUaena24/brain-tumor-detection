8fa51d63e524a53096cc948692d58fd82a72bdcb
juancwu <46619361+juancwu@users.noreply.github.com>
2023-03-23 19:10:22 -0400

    resolve conflicts

--- src/models/vgg19_model.py
+++ src/models/vgg19_model.py
@@ -1,35 +1,12 @@
+from keras.layers import Dropout, GlobalAveragePooling2D, Dense 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import cv2
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from tensorflow.keras.models import Model
 
+from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 from keras.applications.vgg19 import VGG19
-
-random_seed = 42
-img_size = 244
-current_dir = os.path.dirname(os.path.abspath(__file__))
-data_dir = os.path.join(current_dir, '../../data/raw/brain_tumor')
-classes = ["no", "yes"]
-
-X = []
-Y = []
-for cl in classes:
-    folder = os.path.join(data_dir, cl)
-    for img in os.listdir(folder):
-        resized_img = cv2.resize(cv2.imread(
-            os.path.join(folder, img)), (img_size, img_size))
-        X.append(resized_img)
-        Y.append(0 if cl == "no" else 1)
-
-X = np.array(X)
-Y = np.array(Y)
-Y = to_categorical(Y, num_classes=2)
-train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.2, random_state=random_seed)
 
 vgg19_base = VGG19(weights='imagenet', include_top=False,
                    input_shape=(img_size, img_size, 3))
@@ -65,3 +42,21 @@ print("loss:", score[0])
 print("accuracy:", score[1])
 
 # model.save("/some/path")
+
+class DetectorModelBaseVGG19:
+    def __init__(self, weights='imagenet', include_top=False, input_shape=(224, 224, 3), classes=["no", "yes"]):
+        self.vgg19 = VGG19(weights=weights, include_top=include_top, input_shape=input_shape)
+        self.classes = [] 
+        
+        # create new layers with vgg19 as a base
+        x = self.vgg19.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+        x = Dropout(0.5)(x)
+
+        predictions = Dense(len(self.classes), activation='softmax')(x)
+    
+    def fit(train_X, trian_Y, test_X, test_Y, batch_size=32, epochs=30, verbose=1):
+        pass
+
+
