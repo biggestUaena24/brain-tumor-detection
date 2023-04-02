1262388f37af6738a154fb90d2a91bc3badd6d65
Geun Han Chung <97194224+biggestUaena24@users.noreply.github.com>
2023-04-01 21:37:24 -0400

    Update main.py
    everything set in this file

--- src/main.py
+++ src/main.py
@@ -1,54 +1,39 @@
-import os
-import cv2
-import numpy as np
 from sklearn.model_selection import train_test_split
 from models.resnet_model import DetectorModelBaseResNet
 from models.vgg19_model import DetectorModelBaseVGG19
+from data.make_dataset import load_data
 
 random_seed = 42
 
 
-def load_data(random_state, test_size=0.2, from_path=None):
-    if from_path is None:
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        data_dir = os.path.join(current_dir, '../data/processed/brain_tumor')
-    else:
-        data_dir = from_path
-    classes = ["no", "yes"]
-
-    X = []
-    Y = []
-    for cl in classes:
-        folder = os.path.join(data_dir, cl)
-        for img in os.listdir(folder):
-            img_data = cv2.imread(os.path.join(folder, img))
-            X.append(img_data)
-            Y.append(0 if cl == "no" else 1)
-
-    X = np.array(X)
-    Y = np.array(Y)
-
-    return train_test_split(X, Y, test_size=test_size, random_state=random_state)
-
-
-train_X, test_X, train_Y, test_Y = load_data(random_seed)
+X, Y = load_data(target="train")
+train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.2, random_state=random_seed)
 
 param_grid_resnet = {
     'optimizer': ['adam', 'rmsprop'],
     'loss': ['categorical_crossentropy'],
-    'batch_size': [32, 64]
+    'batch_size': [32, 64],
+    'epochs': [30],
 }
 
 param_grid_vgg = {
     'optimizer': ['adam', 'rmsprop'],
     'loss': ['binary_crossentropy'],
-    'batch_size': [32, 64]
+    'batch_size': [32, 64],
+    'epochs': [30],
 }
-
-vgg_model = DetectorModelBaseVGG19()
-vgg_history, vgg_best_params = vgg_model.grid_search(
-    param_grid_vgg, train_X, train_Y, test_X, test_Y, epochs=10)
 
 resnet_model = DetectorModelBaseResNet()
 resnet_history, resnet_best_params = resnet_model.grid_search(
     param_grid_resnet, train_X, train_Y, test_X, test_Y, epochs=10)
+resnet_model.plot_accuracy()
+resnet_model.plot_roc_curve(test_X, test_Y)
+resnet_model.plot_confusion_matrix(test_X, test_Y)
+
+vgg_model = DetectorModelBaseVGG19()
+vgg_history, vgg_best_params = vgg_model.grid_search(
+    param_grid_vgg, train_X, train_Y, test_X, test_Y, epochs=10)
+vgg_model.plot_accuracy()
+vgg_model.plot_roc_curve(test_X, test_Y)
+vgg_model.plot_confusion_matrix(test_X, test_Y)
+
